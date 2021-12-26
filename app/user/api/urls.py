cbe5e04d06bab76c0adb27d89a8c06b00220b69f
HSAkash <44114317+HSAkash@users.noreply.github.com>
2021-12-26 06:58:29 +0600

    Update urls.py

--- app/user/api/urls.py
+++ app/user/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+
 from . import views
 
 app_name = 'api_user'
@@ -7,4 +10,5 @@ urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
     path('login/', views.MyTokenObtainPairView.as_view(), name='api_login'),
     path('manage/', views.ManagerUserView.as_view(), name='manage'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
